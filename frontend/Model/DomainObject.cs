aac8c0af41184a222d092112f30fd6e10b4cef5e
Julian Hüppauff <julian@hueppauff.info>
2020-10-20 19:06:37 +0200

    added dynamic domain loading, error handling

---
+++ frontend/Model/DomainObject.cs
@@ -0,0 +1,11 @@
+namespace frontend.Model
+{
+    public class DomainObject
+    {
+        public string ParitionKey { get; set; }
+
+        public string RowKey { get; set; }
+
+        public string Domain { get; set; }
+    }
+}
