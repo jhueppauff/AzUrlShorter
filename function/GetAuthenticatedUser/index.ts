2500f6135f4221d804a6c28019eaed0d8f6229da
Julian Hüppauff <julian@hueppauff.info>
2020-10-20 20:40:53 +0200

    added user function

---
+++ function/GetAuthenticatedUser/index.ts
@@ -0,0 +1,19 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('GetDomain function processed a request.');
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Hello " + (req.query.name || req.body.name)+". CurrentUser:"+req.headers['x-ms-client-principal-name']
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+};
+
+export default httpTrigger;
