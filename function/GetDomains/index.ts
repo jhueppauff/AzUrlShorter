b714c79e5220b31a2c39a048612243c401b034c0
Julian Hüppauff <julian@hueppauff.info>
2020-10-22 22:20:39 +0200

    updated response

--- function/GetDomains/index.ts
+++ function/GetDomains/index.ts
@@ -6,6 +6,13 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     if (req.headers['X-MS-CLIENT-PRINCIPAL-ID'] != null) {
         context.res.status(200).json(context.bindings.configuration);   
     }
+    else
+    {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
 };
 
 export default httpTrigger;
