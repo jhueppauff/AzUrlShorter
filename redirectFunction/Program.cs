89fe5111acb4653550c2b7ecc7006c57e0adf7ff
Julian Hüppauff <julian@hueppauff.com>
2024-09-26 23:26:26 +0200

    added logging rules

--- redirectFunction/Program.cs
+++ redirectFunction/Program.cs
@@ -1,12 +1,36 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Linq;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services => {
+    .ConfigureAppConfiguration((context, builder) =>
+    {
+        builder.SetBasePath(context.HostingEnvironment.ContentRootPath);
+        builder.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true);
+        builder.AddJsonFile("host.json", optional: true, reloadOnChange: true);
+        builder.AddEnvironmentVariables();
+    })
+    .ConfigureServices(services =>
+    {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+    })
+    .ConfigureLogging(logging =>
+    {
+        // The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs. Application Insights requires an explicit override. 
+        // Configuration in hosts did not override
+        logging.Services.Configure<LoggerFilterOptions>(options =>
+        {
+            LoggerFilterRule? defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+            if (defaultRule is not null)
+            {
+                options!.Rules.Remove(defaultRule);
+            }
+        });
     })
     .Build();
 
